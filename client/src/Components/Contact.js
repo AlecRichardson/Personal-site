56d7a6aa33148dc8d6e4adaffd93e4a1d9eced72
Alec Richardson <alec.richardson@mail.missouri.edu>
2018-10-05 18:00:04 -0500

    switched message input to text area

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 import "./Css/Contact.css";
 
@@ -7,32 +7,34 @@ import { Container, Divider, Form, Button } from "semantic-ui-react";
 
 export default class Contact extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
-       email: '',
-       subject: '',
-       message: ''
-    }
+      email: "",
+      subject: "",
+      message: ""
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleEmailSubmit = this.handleEmailSubmit.bind(this);
   }
 
   handleChange = e => {
-    this.setState({[e.target.name]: e.target.value })
-  }
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   handleEmailSubmit = e => {
     e.preventDefault();
 
     const { email, subject, message } = this.state;
 
-    const form = await axios.post('/api/form', {
-     email, subject, message
-    })
-  }
-  
+    const form = axios.post("/api/form", {
+      email,
+      subject,
+      message
+    });
+  };
+
   render() {
     return (
       <Container className="contact-container" textAlign="center">
@@ -47,21 +49,35 @@ export default class Contact extends Component {
             <Form onSubmit={this.handleEmailSubmit}>
               <Form.Field>
                 <label>Email</label>
-                <input placeholder="Email" type ='email' name='email' onChange={this.handleChange}/>
+                <input
+                  placeholder="Email"
+                  type="email"
+                  name="email"
+                  onChange={this.handleChange}
+                />
               </Form.Field>
               <Form.Field>
                 <label>Subject</label>
-                <input placeholder="Subject" type ='text' name='subject' onChange={this.handleChange} />
+                <input
+                  placeholder="Subject"
+                  type="text"
+                  name="subject"
+                  onChange={this.handleChange}
+                />
               </Form.Field>
               <Form.Field>
                 <label>Message</label>
-                <input placeholder="message" type="textarea" name='message' onChange={this.handleChange}/>
+                <textarea
+                  placeholder="message"
+                  type="textarea"
+                  name="message"
+                  onChange={this.handleChange}
+                />
               </Form.Field>
-              
-                <Button type="submit" color="blue">
-                  Submit
-                </Button>
-             
+
+              <Button type="submit" color="blue">
+                Submit
+              </Button>
             </Form>
           </Container>
         </div>
